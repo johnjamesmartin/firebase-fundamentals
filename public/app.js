9aaa00acd4bf86b8cf2d2a2ff4cf3043270bba13
John Martin <johnjamesmartin@live.com>
2019-07-02 01:15:33 +0100

    Implement onSnapshot method for posts

--- public/app.js
+++ public/app.js
@@ -1,9 +1,26 @@
 document.addEventListener('DOMContentLoaded', event => {
   const app = firebase.app();
+  const db = firebase.firestore();
+  const myPost = db.collection('posts').doc('firstpost');
+
+  // onSnapshot method is like "get" but updates in real time:
+  myPost.onSnapshot(doc => {
+    const data = doc.data();
+    document.write(data.title + '<br>');
+  });
+
   console.log(app);
 });
 
 function googleLogin() {
   const provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider);
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(result => {
+      const user = result.user;
+      document.write(`Hello ${user.displayName}`);
+      console.log(user);
+    })
+    .catch(console.log);
 }
