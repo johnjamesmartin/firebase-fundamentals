e171b1ea710b7349f19d8b13650023cf023e9c11
John Martin <johnjamesmartin@live.com>
2019-07-02 01:27:20 +0100

    Retrieve from database

--- public/app.js
+++ public/app.js
@@ -1,16 +1,41 @@
 document.addEventListener('DOMContentLoaded', event => {
   const app = firebase.app();
   const db = firebase.firestore();
+  const productsRef = db.collection('products');
+  const query = productsRef.where('price', '>', 4);
+  //const query = productsRef.orderBy('price', 'desc').limit(2);
+  query.get().then(products => {
+    products.forEach(doc => {
+      data = doc.data();
+      document.write(`${data.name} at ${data.price}<br/>`);
+    });
+  });
+});
+
+/*
+document.addEventListener('DOMContentLoaded', event => {
+  const app = firebase.app();
+  const db = firebase.firestore();
   const myPost = db.collection('posts').doc('firstpost');
 
   // onSnapshot method is like "get" but updates in real time:
   myPost.onSnapshot(doc => {
     const data = doc.data();
-    document.write(data.title + '<br>');
+    document.querySelector('#title').innerHTML = data.title;
   });
 
   console.log(app);
 });
+
+function updatePost(e) {
+  const db = firebase.firestore();
+  const myPost = db.collection('posts').doc('firstpost');
+  myPost.update({ title: e.target.value });
+}
+*/
+
+/*
+// How we'd log in with button:
 
 function googleLogin() {
   const provider = new firebase.auth.GoogleAuthProvider();
@@ -24,3 +49,4 @@ function googleLogin() {
     })
     .catch(console.log);
 }
+*/
